f7023ae955330223c5a3365e13ae92d7005356e3
AadOfzo <aadofzo@gmail.com>
2022-06-20 23:40:32 +0200

    Tweede Commit

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    @Test
+    public void getName() {
+        //Arrange
+        Person test = new Person("Adrien" , "L'Honoré Naber" , "Male" , "36");
+        // Act
+        String result = test.getName();
+        // Assert
+        Assertions.assertEquals("Adrien", result);
+    }
+
+    @Test
+    public void getFather(){
+        Person test = new Person("Adrien" , "L'Honoré Naber" , "Male" , "36");
+        Person testFather = new Person("Henri" , "L'Honoré Naber" , "Male" , "76")
+        test.setFather(testFather);
+
+        Person result = test.getFather();
+
+        Assertions.assertEquals(testFather , result);
+
+    }
+}
